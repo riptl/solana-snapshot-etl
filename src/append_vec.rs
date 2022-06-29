2bfab49770632ace95b32f6fbc0db9b575da845b
Richard Patel <me@terorie.dev>
2022-06-29 17:24:42 +0200

    append_vec: remove write functionality

--- src/append_vec.rs
+++ src/append_vec.rs
@@ -21,21 +21,17 @@ use {
     serde::{Deserialize, Serialize},
     solana_sdk::{
         account::{Account, AccountSharedData, ReadableAccount},
-        clock::{Epoch, Slot},
+        clock::{Epoch},
         hash::Hash,
         pubkey::Pubkey,
     },
     std::{
-        borrow::Borrow,
         convert::TryFrom,
-        fs::{remove_file, OpenOptions},
-        io::{self, Seek, SeekFrom, Write},
+        fs::OpenOptions,
+        io,
         mem,
-        path::{Path, PathBuf},
-        sync::{
-            atomic::{AtomicUsize, Ordering},
-            Mutex,
-        },
+        path::Path,
+        sync::atomic::{AtomicUsize, Ordering},
     },
 };
 
@@ -178,100 +174,18 @@ impl<'a> Iterator for AppendVecAccountsIter<'a> {
 /// are serialized such that only one thread updates the internal `append_lock` at a time. No
 /// restrictions are placed on reading. That is, one may read items from one thread while another
 /// is appending new items.
-#[derive(Debug)]
 pub struct AppendVec {
-    /// The file path where the data is stored.
-    path: PathBuf,
-
     /// A file-backed block of memory that is used to store the data for each appended item.
     map: MmapMut,
-
-    /// A lock used to serialize append operations.
-    append_lock: Mutex<()>,
 
     /// The number of bytes used to store items, not the number of items.
     current_len: AtomicUsize,
 
     /// The number of bytes available for storing items.
     file_size: u64,
-
-    /// True if the file should automatically be deleted when this AppendVec is dropped.
-    remove_on_drop: bool,
-}
-
-impl Drop for AppendVec {
-    fn drop(&mut self) {
-        if self.remove_on_drop {
-            if let Err(_e) = remove_file(&self.path) {
-                // promote this to panic soon.
-                // disabled due to many false positive warnings while running tests.
-                // blocked by rpc's upgrade to jsonrpc v17
-                //error!("AppendVec failed to remove {:?}: {:?}", &self.path, e);
-            }
-        }
-    }
 }
 
 impl AppendVec {
-    pub fn new(file: &Path, create: bool, size: usize) -> Self {
-        let initial_len = 0;
-        AppendVec::sanitize_len_and_size(initial_len, size).unwrap();
-
-        if create {
-            let _ignored = remove_file(file);
-        }
-
-        let mut data = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(create)
-            .open(file)
-            .map_err(|e| {
-                panic!(
-                    "Unable to {} data file {} in current dir({:?}): {:?}",
-                    if create { "create" } else { "open" },
-                    file.display(),
-                    std::env::current_dir(),
-                    e
-                );
-            })
-            .unwrap();
-
-        // Theoretical performance optimization: write a zero to the end of
-        // the file so that we won't have to resize it later, which may be
-        // expensive.
-        data.seek(SeekFrom::Start((size - 1) as u64)).unwrap();
-        data.write_all(&[0]).unwrap();
-        data.seek(SeekFrom::Start(0)).unwrap();
-        data.flush().unwrap();
-
-        //UNSAFE: Required to create a Mmap
-        let map = unsafe { MmapMut::map_mut(&data) };
-        let map = map.unwrap_or_else(|e| {
-            error!(
-                "Failed to map the data file (size: {}): {}.\n
-                    Please increase sysctl vm.max_map_count or equivalent for your platform.",
-                size, e
-            );
-            std::process::exit(1);
-        });
-
-        AppendVec {
-            path: file.to_path_buf(),
-            map,
-            // This mutex forces append to be single threaded, but concurrent with reads
-            // See UNSAFE usage in `append_ptr`
-            append_lock: Mutex::new(()),
-            current_len: AtomicUsize::new(initial_len),
-            file_size: size as u64,
-            remove_on_drop: true,
-        }
-    }
-
-    pub fn set_no_remove_on_drop(&mut self) {
-        self.remove_on_drop = false;
-    }
-
     fn sanitize_len_and_size(current_len: usize, file_size: usize) -> io::Result<()> {
         if file_size == 0 {
             Err(std::io::Error::new(
@@ -296,17 +210,6 @@ impl AppendVec {
         }
     }
 
-    pub fn flush(&self) -> io::Result<()> {
-        self.map.flush()
-    }
-
-    pub fn reset(&self) {
-        // This mutex forces append to be single threaded, but concurrent with reads
-        // See UNSAFE usage in `append_ptr`
-        let _lock = self.append_lock.lock().unwrap();
-        self.current_len.store(0, Ordering::Release);
-    }
-
     /// how many more bytes can be stored in this append vec
     pub fn remaining_bytes(&self) -> u64 {
         (self.capacity()).saturating_sub(self.len() as u64)
@@ -322,10 +225,6 @@ impl AppendVec {
 
     pub fn capacity(&self) -> u64 {
         self.file_size
-    }
-
-    pub fn file_name(slot: Slot, id: impl std::fmt::Display) -> String {
-        format!("{}.{}", slot, id)
     }
 
     pub fn new_from_file<P: AsRef<Path>>(path: P, current_len: usize) -> io::Result<(Self, usize)> {
@@ -348,12 +247,9 @@ impl AppendVec {
         };
 
         let new = AppendVec {
-            path: path.as_ref().to_path_buf(),
             map,
-            append_lock: Mutex::new(()),
             current_len: AtomicUsize::new(current_len),
             file_size,
-            remove_on_drop: true,
         };
 
         let (sanitized, num_accounts) = new.sanitize_layout_and_length();
@@ -408,44 +304,6 @@ impl AppendVec {
         ))
     }
 
-    /// Copy `len` bytes from `src` to the first 64-byte boundary after position `offset` of
-    /// the internal buffer. Then update `offset` to the first byte after the copied data.
-    fn append_ptr(&self, offset: &mut usize, src: *const u8, len: usize) {
-        let pos = u64_align!(*offset);
-        let data = &self.map[pos..(pos + len)];
-        //UNSAFE: This mut append is safe because only 1 thread can append at a time
-        //Mutex<()> guarantees exclusive write access to the memory occupied in
-        //the range.
-        unsafe {
-            let dst = data.as_ptr() as *mut u8;
-            std::ptr::copy(src, dst, len);
-        };
-        *offset = pos + len;
-    }
-
-    /// Copy each value in `vals`, in order, to the first 64-byte boundary after position `offset`.
-    /// If there is sufficient space, then update `offset` and the internal `current_len` to the
-    /// first byte after the copied data and return the starting position of the copied data.
-    /// Otherwise return None and leave `offset` unchanged.
-    fn append_ptrs_locked(&self, offset: &mut usize, vals: &[(*const u8, usize)]) -> Option<usize> {
-        let mut end = *offset;
-        for val in vals {
-            end = u64_align!(end);
-            end += val.1;
-        }
-
-        if (self.file_size as usize) < end {
-            return None;
-        }
-
-        let pos = u64_align!(*offset);
-        for val in vals {
-            self.append_ptr(offset, val.0, val.1)
-        }
-        self.current_len.store(*offset, Ordering::Release);
-        Some(pos)
-    }
-
     /// Return a reference to the type at `offset` if its data doesn't overrun the internal buffer.
     /// Otherwise return None. Also return the offset of the first byte after the requested data
     /// that falls on a 64-byte boundary.
@@ -477,78 +335,5 @@ impl AppendVec {
             },
             next,
         ))
-    }
-
-    pub fn get_path(&self) -> PathBuf {
-        self.path.clone()
-    }
-
-    /// Return account metadata for each account, starting from `offset`.
-    pub fn accounts(&self, mut offset: usize) -> Vec<StoredAccountMeta> {
-        let mut accounts = vec![];
-        while let Some((account, next)) = self.get_account(offset) {
-            accounts.push(account);
-            offset = next;
-        }
-        accounts
-    }
-
-    /// Copy each account metadata, account and hash to the internal buffer.
-    /// Return the starting offset of each account metadata.
-    /// After each account is appended, the internal `current_len` is updated
-    /// and will be available to other threads.
-    pub fn append_accounts(
-        &self,
-        accounts: &[(StoredMeta, Option<&impl ReadableAccount>)],
-        hashes: &[impl Borrow<Hash>],
-    ) -> Vec<usize> {
-        let _lock = self.append_lock.lock().unwrap();
-        let mut offset = self.len();
-        let mut rv = Vec::with_capacity(accounts.len());
-        for ((stored_meta, account), hash) in accounts.iter().zip(hashes) {
-            let meta_ptr = stored_meta as *const StoredMeta;
-            let account_meta = AccountMeta::from(*account);
-            let account_meta_ptr = &account_meta as *const AccountMeta;
-            let data_len = stored_meta.data_len as usize;
-            let data_ptr = account
-                .map(|account| account.data())
-                .unwrap_or_default()
-                .as_ptr();
-            let hash_ptr = hash.borrow().as_ref().as_ptr();
-            let ptrs = [
-                (meta_ptr as *const u8, mem::size_of::<StoredMeta>()),
-                (account_meta_ptr as *const u8, mem::size_of::<AccountMeta>()),
-                (hash_ptr as *const u8, mem::size_of::<Hash>()),
-                (data_ptr, data_len),
-            ];
-            if let Some(res) = self.append_ptrs_locked(&mut offset, &ptrs) {
-                rv.push(res)
-            } else {
-                break;
-            }
-        }
-
-        // The last entry in this offset needs to be the u64 aligned offset, because that's
-        // where the *next* entry will begin to be stored.
-        rv.push(u64_align!(offset));
-
-        rv
-    }
-
-    /// Copy the account metadata, account and hash to the internal buffer.
-    /// Return the starting offset of the account metadata.
-    /// After the account is appended, the internal `current_len` is updated.
-    pub fn append_account(
-        &self,
-        storage_meta: StoredMeta,
-        account: &AccountSharedData,
-        hash: Hash,
-    ) -> Option<usize> {
-        let res = self.append_accounts(&[(storage_meta, Some(account))], &[&hash]);
-        if res.len() == 1 {
-            None
-        } else {
-            res.first().cloned()
-        }
     }
 }
